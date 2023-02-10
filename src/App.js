8f97a0d527ff6f37d0b25c6b49c94b622e335dd7
Ali Kaan Kiriş <akkiris@gmail.com>
2023-02-11 01:43:45 +0300

    fix: google map address on table at monitoring page.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Sender from "./containers/Sender";
 function App() {
   return (
     <div className="App">
-      <Sender />
+      {/* <Sender /> */}
       {/* <Monitoring /> */}
-      {/* <Delivery /> */}
+      <Delivery />
     </div>
   );
 }
